06273614c7b8c024e65b038e401f2a4c625bd1ec
blaze-darck <paredesgabriel784@gmail.com>
2025-10-28 18:18:14 -0400

    fat:Adicion de la tabla personas

---
+++ src/models/persona.js
@@ -0,0 +1,47 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Persona = sequelize.define("Persona", {
+  nombres: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  apellido_patermo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  apellido_patermo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  fecha_nacimiento: {
+    type: DataTypes.DATEONLY,
+  },
+  telefono: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+
+  // Campos de auditoría
+  created_by: {
+    type: DataTypes.STRING,
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  updated_by: {
+    type: DataTypes.STRING,
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+  },
+  deleted: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+module.exports = Persona;
