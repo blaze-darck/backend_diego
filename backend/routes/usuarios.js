847140186a045b408e2186b993bf526765decb92
AmhedOnil <ino555amhed555@gmail.com>
2025-10-29 07:47:25 -0400

    remove:Eliminar archivos duplicados y mejorados

--- backend/routes/usuarios.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { registrarUsuario } from "../controllers/usuarios.controller.js";
-
-const router = Router();
-
-router.post("/register", registrarUsuario);
-
-export default router;
